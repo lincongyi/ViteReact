b7d853fe0ba24fa67bb30da5c859a63992ce89ac
lincongyi <88583354@163.com>
2022-10-27 16:17:45 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'reset-css';
+import './App.scss'
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      app
+    </div>
+  )
+}
+
+export default App
