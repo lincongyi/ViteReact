1e79c05a6c7fa9de07f5e46ffcc6d71bedcf30aa
congyi <88583354@163.com>
2022-11-29 00:45:18 +0800

    引入菜单

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,11 +1,12 @@
 // 路由鉴权
 // 高阶组件：把一个组件当作另外一个组件的参数传入，并返回新的组件
 import { useStore } from '@stores/index'
+import { getToken } from '@utils/token'
 import { Navigate } from 'react-router-dom'
 
 const Auth = ({ children }: any) => {
   let { loginStore } = useStore()
-  const token = loginStore.getToken()
+  const token = loginStore.getToken() || getToken()
 
   if (token) {
     return <>{children}</>
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,19 +1,38 @@
-import { Button } from 'antd'
-import { request } from '@utils/request'
+import { Menu } from 'antd'
+import type { MenuProps } from 'antd'
+import { useState } from 'react'
 
 const Layout = () => {
-  const handleEvent = () => {
-    request
-      .post('/login', { username: 'admin', password: 'admin' })
-      .then(({ data }) => console.log(data))
+  const items = [
+    { label: '菜单项一', key: 'item-1' }, // 菜单项务必填写 key
+    { label: '菜单项二', key: 'item-2' },
+    {
+      label: '子菜单1',
+      key: 'submenu-1',
+      children: [{ label: '子菜单项1-1', key: 'submenu-item-1-1' }],
+    },
+    {
+      label: '子菜单2',
+      key: 'submenu-2',
+      children: [{ label: '子菜单项2-1', key: 'submenu-item-2-1' }],
+    },
+  ]
+  const [current, setCurrent] = useState('item-2')
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log(e)
+    setCurrent(e.key)
   }
   return (
-    <div>
-      <Button type="primary" onClick={() => handleEvent()}>
-        Primary Button
-      </Button>
-      Layout
-    </div>
+    <>
+      <Menu
+        theme="dark"
+        mode="inline"
+        items={items}
+        selectedKeys={[current]}
+        onClick={onClick}
+      />
+    </>
   )
 }
 
