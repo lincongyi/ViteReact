b53d69900aeba98fe1d62feaa68e14e33e23471a
congyi <88583354@163.com>
2022-11-30 01:15:15 +0800

    update

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,34 +1,46 @@
 import React, { useState } from 'react'
-import { Menu, Layout } from 'antd'
+import { Menu, Layout, Popconfirm } from 'antd'
 import type { MenuProps } from 'antd'
 import {
+  PoweroffOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   AppstoreOutlined,
+  ProfileOutlined,
+  FileDoneOutlined,
+  TagsOutlined,
 } from '@ant-design/icons'
 import './index.scss'
 
 const { Header, Sider, Content } = Layout
 
+const text = '是否确认退出登录？'
+
 const AppLayout: React.FC = () => {
+  // 退出登录
+  const onConfirm = () => {}
+
   const [collapsed, setCollapsed] = useState(false)
 
   const items = [
     { label: '菜单项一', key: 'item-1', icon: <AppstoreOutlined /> }, // 菜单项务必填写 key
-    { label: '菜单项二', key: 'item-2' },
+    { label: '菜单项二', key: 'item-2', icon: <ProfileOutlined /> },
     {
       label: '子菜单1',
       key: 'submenu-1',
+      icon: <FileDoneOutlined />,
       children: [{ label: '子菜单项1-1', key: 'submenu-item-1-1' }],
     },
     {
       label: '子菜单2',
       key: 'submenu-2',
+      icon: <TagsOutlined />,
       children: [{ label: '子菜单项2-1', key: 'submenu-item-2-1' }],
     },
   ]
   const [current, setCurrent] = useState('item-2')
 
+  // 切换菜单
   const onClick: MenuProps['onClick'] = (e) => {
     console.log(e)
     setCurrent(e.key)
@@ -56,6 +68,21 @@ const AppLayout: React.FC = () => {
               onClick: () => setCollapsed(!collapsed),
             }
           )}
+          <div className="user-tool">
+            user name
+            <Popconfirm
+              placement="bottomRight"
+              title={text}
+              onConfirm={onConfirm}
+              okText="确定"
+              cancelText="取消"
+            >
+              <div className="logoff">
+                <PoweroffOutlined />
+                退出登录
+              </div>
+            </Popconfirm>
+          </div>
         </Header>
         <Content className="content">Content</Content>
       </Layout>
