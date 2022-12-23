352d90ae28b54a5e48a8723c5dbdba287f00f73a
lincongyi <88583354@163.com>
2022-12-23 15:09:28 +0800

    完善内容管理模块

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -11,7 +11,10 @@ import {
   Table,
   Image,
   Empty,
+  Space,
+  Modal,
 } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
 import './index.scss'
 import locale from 'antd/es/date-picker/locale/zh_CN'
 import 'moment/dist/locale/zh-cn'
@@ -19,8 +22,10 @@ import moment from 'moment'
 import type { ColumnsType } from 'antd/es/table'
 import { getArticleType, getArticleList } from '@api/article'
 
-type statusType = '0' | '1' | '2' | '3'
+type statusType = 0 | 1 | 2 | 3
+type varietyType = '0' | '1' | '2' | '3'
 const { RangePicker } = DatePicker
+const { confirm } = Modal
 
 // 表格栏目标题
 interface IcolumnsType {
@@ -28,10 +33,13 @@ interface IcolumnsType {
   cover: string
   title: string
   status: string
+  type: string
   publishDate: string
   readTimes: number
   commentTimes: number
 }
+
+const statusList = ['全部', '未发布', '已发布', '待审核', '已删除']
 
 const columns: ColumnsType<IcolumnsType> = [
   {
@@ -46,11 +54,52 @@ const columns: ColumnsType<IcolumnsType> = [
     align: 'center',
   },
   { title: '标题', dataIndex: 'title' },
-  { title: '发布状态', dataIndex: 'status' },
+  {
+    title: '发布状态',
+    dataIndex: 'status',
+    render: status => statusList[Number(status)],
+  },
   { title: '发布日期', dataIndex: 'publishDate' },
   { title: '阅读数', dataIndex: 'readTimes' },
   { title: '评论数', dataIndex: 'commentTimes' },
+  {
+    title: '操作',
+    render: props => {
+      return (
+        <Space>
+          <Button type='primary' onClick={() => onDetail(props)}>
+            详情
+          </Button>
+          <Button type='text' danger onClick={() => onDelete(props.id)}>
+            删除
+          </Button>
+        </Space>
+      )
+    },
+  },
 ]
+
+// 表格单项详情
+const onDetail = (item: Record<string, any>) => {
+  console.log(item)
+}
+
+// 表格单项删除
+const onDelete = (id: string) => {
+  confirm({
+    title: '确认删除',
+    icon: <ExclamationCircleOutlined />,
+    content: '是否确认删除该项？',
+    okText: '确认',
+    cancelText: '取消',
+    onOk () {
+      console.log('确认')
+    },
+    onCancel () {
+      console.log('取消')
+    },
+  })
+}
 
 const Article: React.FC = () => {
   const [articleType, setArticleType] = useState([]) // 文章类型
@@ -73,15 +122,15 @@ const Article: React.FC = () => {
     })()
   }, [])
 
-  const [status, setStatus] = useState<statusType>('2') // 发布状态
-  const [variety, setVariety] = useState<statusType>('0') // 文章类型
+  const [status, setStatus] = useState<statusType>(0) // 发布状态
+  const [variety, setVariety] = useState<varietyType>('0') // 文章类型
 
   const onValuesChange = ({
     status,
     variety,
   }: {
     status: statusType
-    variety: statusType
+    variety: varietyType
   }) => {
     setStatus(status)
     setVariety(variety)
@@ -94,6 +143,7 @@ const Article: React.FC = () => {
     // console.log(startDate.format('yyyy-MM-DD'))
     // console.log(endDate.format('yyyy-MM-DD'))
   }
+
   return (
     <>
       <Form
@@ -108,37 +158,38 @@ const Article: React.FC = () => {
       >
         <Row gutter={16}>
           <Col span={6}>
-            <Form.Item label="文章标题" name="title">
+            <Form.Item label='文章标题' name='title'>
               <Input />
             </Form.Item>
           </Col>
           <Col span={6}>
-            <Form.Item label="文章标题" name="title">
+            <Form.Item label='文章标题' name='title'>
               <Input />
             </Form.Item>
           </Col>
           <Col span={6}>
-            <Form.Item label="文章标题" name="title">
+            <Form.Item label='文章标题' name='title'>
               <Input />
             </Form.Item>
           </Col>
           <Col span={6}>
-            <Form.Item label="DatePicker" name="datePicker">
+            <Form.Item label='DatePicker' name='datePicker'>
               <DatePicker locale={locale} />
             </Form.Item>
           </Col>
           <Col span={6}>
-            <Form.Item label="发布状态" name="status">
+            <Form.Item label='发布状态' name='status'>
               <Radio.Group>
-                <Radio.Button value="0">未发布</Radio.Button>
-                <Radio.Button value="1">已发布</Radio.Button>
-                <Radio.Button value="2">待审核</Radio.Button>
-                <Radio.Button value="3">已删除</Radio.Button>
+                {statusList.map((item, index) => (
+                  <Radio.Button value={index} key={index}>
+                    {item}
+                  </Radio.Button>
+                ))}
               </Radio.Group>
             </Form.Item>
           </Col>
           <Col span={6}>
-            <Form.Item label="文章类型" name="variety">
+            <Form.Item label='文章类型' name='variety'>
               <Select>
                 {articleType.map((item: { value: string; label: string }) => (
                   <Select.Option value={item.value} key={item.value}>
@@ -149,20 +200,20 @@ const Article: React.FC = () => {
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="发布区间" name="rangeDate">
+            <Form.Item label='发布区间' name='rangeDate'>
               <RangePicker locale={locale} />
             </Form.Item>
           </Col>
         </Row>
-        <Row justify="end">
-          <Col span={6} className="tr">
-            <Button type="primary" danger htmlType="submit">
+        <Row justify='end'>
+          <Col span={6} className='tr'>
+            <Button type='primary' htmlType='submit'>
               查询
             </Button>
           </Col>
         </Row>
       </Form>
-      <Table rowKey="id" columns={columns} dataSource={articleList} />
+      <Table rowKey='id' columns={columns} dataSource={articleList} />
     </>
   )
 }
