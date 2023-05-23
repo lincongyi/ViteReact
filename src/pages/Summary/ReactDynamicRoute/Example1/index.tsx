95ce06e3cbe540f067fcd9ee0c751bf51ca10167
lincongyi <88583354@163.com>
2023-05-23 18:32:37 +0800

    bug fixed

--- src/pages/Summary/ReactDynamicRoute/Example1/index.tsx
+++ src/pages/Summary/ReactDynamicRoute/Example1/index.tsx
@@ -1,31 +1,33 @@
 import React, { useContext } from 'react'
 import { Button, Space } from 'antd'
 import { useNavigate } from 'react-router-dom'
-import { context } from '@/App'
-import { lazyLoad } from '@/router'
+import { context } from '@/context'
+import { lazyLoad } from '@/utils'
 
 const Example1 = () => {
   const navigate = useNavigate()
 
-  const { dispatchAuthRoute } = useContext(context)!
+  const { dispatchRoute } = useContext(context)!
 
-  return <Space direction='vertical'>
-  <Button type='primary' onClick={() => navigate('/authRoute')}>
-    路由跳转
-  </Button>
-  <Button
-    onClick={() =>
-      dispatchAuthRoute([
-        {
-          path: 'authRoute',
-          element: lazyLoad('authRoute'),
-        },
-      ])
-    }
-  >
-    添加路由权限
-  </Button>
-</Space>
+  return (
+    <Space direction='vertical'>
+      <Button type='primary' onClick={() => navigate('/authRoute')}>
+        路由跳转
+      </Button>
+      <Button
+        onClick={() =>
+          dispatchRoute([
+            {
+              path: 'authRoute',
+              element: lazyLoad('AuthRoute'),
+            },
+          ])
+        }
+      >
+        添加路由权限
+      </Button>
+    </Space>
+  )
 }
 
 export default Example1
