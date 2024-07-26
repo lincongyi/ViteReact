2b59f8bc3718870715648c19c672ee08bf812d7e
lincongyi <88583354@163.com>
2024-07-26 16:35:56 +0800

    feat: antd v5样式兼容

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  legacyLogicalPropertiesTransformer,
+  StyleProvider,
+} from '@ant-design/cssinjs'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <App />
+  <StyleProvider
+    hashPriority="high"
+    transformers={[legacyLogicalPropertiesTransformer]}
+  >
+    <App />
+  </StyleProvider>
   // </React.StrictMode>
 )
